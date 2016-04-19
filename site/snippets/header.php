36b113162683462f2f2e232a32d9d7adca1afd70
Anastasios Orfanidis <AOrfanidis@tagworldwide.com>
2016-04-19 08:19:56 +0100

    Added google analytics html + in the head + delete thumbs

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -20,7 +20,16 @@
   <?php snippet('favicons') ?>
 
   <?php echo css('assets/css/main.min.css') ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+    ga('create', 'UA-76534146-2', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 </head>
 <body id="<?php echo $page->id(); ?>" class="no-js <?php echo $page->template() ?>">
   <div id="fb-root"></div>
